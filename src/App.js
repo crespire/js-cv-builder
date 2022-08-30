1f63337af0851869042c7144fd2be0909852f09b
crespire <li.simmon@gmail.com>
2022-08-30 16:56:59 -0400

    Update App.js to be class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
+class App extends Component {
+  constructor(props) {
+    super (props);
+
+    this.state = {}
+  }
+
+  render() {
+
+    return (
     <div className="App">
+      
     </div>
-  );
+    );
+  };
 }
 
 export default App;
