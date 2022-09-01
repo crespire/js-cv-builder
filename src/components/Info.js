5d693437f62f8800fc52b44aaa70e9bce74e7a51
crespire <li.simmon@gmail.com>
2022-09-01 15:14:59 -0400

    Add styles to Info component

--- src/components/Info.js
+++ src/components/Info.js
@@ -28,11 +28,11 @@ class InfoContainer extends Component {
   render() {
 
     return (
-      <div className="information">
-        <div className="information-name">
-          <input type='text' name='name' placeholder='Name' id='name' onChange={this.handleInputChange}></input>
+      <div className="container mx-auto p-12 flex justify-around content-center">
+        <div className="place-content-center">
+          <input className="text-3xl" type='text' name='name' placeholder='Name' id='name' onChange={this.handleInputChange}></input>
         </div>
-        <div className='information-details'>
+        <div className='flex-col place-content-evenly space-x-4'>
           <input type='text' name='location' placeholder='Location' id='location' onChange={this.handleInputChange}></input>
           <input type='email' name='email' placeholder='your@email.com' id='email' onChange={this.handleInputChange}></input>
           <input type='tel' name='phone' placeholder='555-555-5555' id='phone' onChange={this.handleInputChange}></input>
