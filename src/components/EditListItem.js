8f58dd5b98acdd2d9c8d5be305c8a4131fbda2c1
crespire <li.simmon@gmail.com>
2022-09-04 17:12:38 -0400

    Update EditListItem to utilize callbacks

--- src/components/EditListItem.js
+++ src/components/EditListItem.js
@@ -2,14 +2,16 @@ import React, { Component } from 'react';
 
 class EditListItem extends Component {
   render() {
-    const { place, date_end, date_start, title, description }= this.props;
+    const { place, title, itemID, handleEditInput, handleEditSubmit, editItem }= this.props;
 
     return (
-      <div>
-        <h3>{place}</h3>
-        <sub>{date_start} - {date_end}</sub>
-        <h6>{title}</h6>
-        <p>{description}</p>
+      <div className="container">
+        <input type="text" name="place" value={editItem.place} placeholder={place} onChange={(event) => { handleEditInput(event, itemID) }}></input>
+        <input type="date" name="date_start" value={editItem.date_start} onChange={(event) => { handleEditInput(event, itemID) }}></input>
+        <input type="date" name="date_end" value={editItem.date_end} onChange={(event) => { handleEditInput(event, itemID) }}></input>
+        <input type="text" name="entry_title" value={editItem.entry_title} placeholder={title} onChange={(event) => { handleEditInput(event, itemID) }}></input>
+        <textarea name="entry_description" value={editItem.entry_description} placeholder='Details...' onChange={(event) => { handleEditInput(event, itemID) }}></textarea>
+        <button onClick={(event) => { handleEditSubmit(event, itemID) }}>Update</button>
       </div>
     );
   }
