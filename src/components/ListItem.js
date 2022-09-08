9f36ef23a502afe77b3a5725fb53b28fe6847110
crespire <li.simmon@gmail.com>
2022-09-08 10:06:46 -0400

    Finish ListItem component styles

--- src/components/ListItem.js
+++ src/components/ListItem.js
@@ -5,13 +5,13 @@ class ListItem extends Component {
     const { itemID, toggleEdit, deleteItem, place, date_end, date_start, title, description }= this.props;
 
     return (
-      <div>
-        <h3>{place}</h3>
-        <sub>{date_start} - {date_end}</sub>
-        <h6>{title}</h6>
-        <p>{description}</p>
-        <button onClick={() => { toggleEdit(itemID)} }>Edit</button>
-        <button onClick={() => { deleteItem(itemID)} }>Delete</button>
+      <div className="container grid grid-cols-10 grid-rows-4 gap-4 border-solid border-black border p-6">
+        <h3 className="col-start-1 row-start-1 col-span-2 row-span-1">{place}</h3>
+        <small className="col-start-1 row-start-2 col-span-2 row-span-1">{date_start} - {date_end}</small>
+        <h6 className="col-start-3 row-start-1 col-span-7 row-span-1">{title}</h6>
+        <div className="col-start-3 row-start-2 col-span-7 row-span-3">{description}</div>
+        <button className="bg-slate-200 col-start-10 row-start-1 col-span-1 row-span-2" onClick={() => { toggleEdit(itemID)} }>Edit</button>
+        <button className="bg-slate-200 col-start-10 row-start-3 col-span-1 row-span-2" onClick={() => { deleteItem(itemID)} }>Delete</button>
       </div>
     );
   }
