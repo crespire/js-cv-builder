8acc3794363ec7f142500fbb5cffcacfe231a3d9
crespire <li.simmon@gmail.com>
2022-09-08 09:51:31 -0400

    Fix info container

--- src/components/Info.js
+++ src/components/Info.js
@@ -28,11 +28,11 @@ class InfoContainer extends Component {
   render() {
 
     return (
-      <div className="flex place-content-between px-12 py-6 print:p-0">
-        <div className="container place-content-center">
+      <div className="flex place-content-evenly p-6 print:p-0">
+        <div>
           <input className="text-3xl" type='text' name='name' placeholder='Name' id='name' onChange={this.handleInputChange}></input>
         </div>
-        <div className='container grid grid-cols-2 gap-4 place-content-center print:place-content-start'>
+        <div className='grid grid-cols-2 gap-4 place-content-center print:place-content-start'>
           <input type='text' name='location' placeholder='Location' id='location' onChange={this.handleInputChange}></input>
           <input type='email' name='email' placeholder='your@email.com' id='email' onChange={this.handleInputChange}></input>
           <input type='tel' name='phone' placeholder='555-555-5555' id='phone' onChange={this.handleInputChange}></input>
