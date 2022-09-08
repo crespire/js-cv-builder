04e3d5839ea9998f161dc1780ae46d0c26f91d4f
crespire <li.simmon@gmail.com>
2022-09-08 10:22:02 -0400

    Add back Job experience component

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
       <div className="App container mx-auto max-w-7xl print:p-0 p-12 flex flex-col justify-center align-center place-content-center space-y-4">
         <InfoContainer />
         <List place='School' title='Degree' />
-        {/* <List place='Company' title='Job Title' /> */}
+        <List place='Company' title='Job Title' />
       </div>
     );
   };
